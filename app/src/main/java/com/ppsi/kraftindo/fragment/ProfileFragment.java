d7cbae8caedaf1863f661069ccaeffae34c7e88c
galaxyan12 <jody@student.untan.ac.id>
2019-11-17 18:21:30 +0700

    Add profile and logout

--- app/src/main/java/com/ppsi/kraftindo/fragment/ProfileFragment.java
+++ app/src/main/java/com/ppsi/kraftindo/fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.ppsi.kraftindo.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +8,24 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.ppsi.kraftindo.LoginActivity;
 import com.ppsi.kraftindo.R;
+import com.ppsi.kraftindo.RegisterActivity;
 
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        CardView cardLogout = v.findViewById(R.id.profile_logout);
+        cardLogout.setOnClickListener(v1 -> {
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        });
+        return v;
     }
 }
